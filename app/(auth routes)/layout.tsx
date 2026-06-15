2429e0fa19ae1dc2c0ef27a48c47606f74e74287
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-06-15 23:32:23 +0200

    change fun name

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,4 +1,4 @@
 
-export default function RootLatout({children}:{children: React.ReactNode}) {
+export default function RootLayout({children}:{children: React.ReactNode}) {
     return(<>{children}</>);
 }
