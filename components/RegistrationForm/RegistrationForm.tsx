c3a43544962f131e887e9fec54528da17a3d3fe2
Viktoriia Veremiichuk <vikaveremiychuk@gmail.com>
2026-06-19 23:41:15 +0300

    Update RegistrationForm.tsx

--- components/RegistrationForm/RegistrationForm.tsx
+++ components/RegistrationForm/RegistrationForm.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { isAxiosError } from "axios";
 import toast from "react-hot-toast";
@@ -26,9 +26,6 @@ const validationSchema = Yup.object({
     .max(128, "Password must be at most 128 characters")
     .matches(/^\S+$/, "Password must not contain spaces")
     .required("Password is required"),
-  name: Yup.string().min(2).max(50).required(),
-  email: Yup.string().email().required(),
-  password: Yup.string().min(8).required(),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")])
     .required(),
