9129f04f54dff4d033276af264d4311ffc7e3dea
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-20 01:27:34 +0200

    fixed styles, added link to modal exit window

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,55 +1,64 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import LogoutModal from "../Logout/LogoutModal/LogoutModal"
 
 import {Inter} from "next/font/google";
 const inter = Inter({subsets: ["latin"]});
 
 export default function Header(){
     const [menuOpen, setMenuOpen] = useState(false);
+    const [logoutOpen, setLogoutOpen] = useState(false);
+    const user = {name: "Jhon", email: "jhon@gmail.com", avatar: null,};
+    const displayName = user.name?.trim()|| user.email.split("@")[0];
+    const avatarLetter = displayName.charAt(0).toUpperCase(); //тимчасова змінна для відображення аватара клієнта
     
-    const user = { name: "Jhon", avatar:null}; //тимчасова змінна для відображення аватара клієнта
+    const pathname = usePathname();
+    const isRecipesActive = pathname === "/" || pathname.startsWith("/recipes");
+    const isLoginActive = pathname.startsWith("/login");
+    const isProfileActive = pathname.startsWith("/profile");
     
-    
-    
-    
-    const isLoggedIn = false;   
+    const isLoggedIn = true;   
     return(
         <header className={`${styles.header} ${inter.className}`}>
-        <div className={styles.logo}>
-            <Image src="/logo.svg" alt="" width={24} height={24}/>
+        <Link href="/" className={styles.logo}>
+            <Image src="/logo.svg" alt="Tasteorama logo" width={24} height={24}/>
             <span>Tasteorama</span>
-        </div>
+        </Link>
 
         {/*Desktop*/}
         <nav className={styles.desktopNav}>
             <div className={styles.navLinks}>
-                <Link href="/recipes">Recipes</Link>
-                {isLoggedIn && <Link href="/recipes">My Profile</Link>}
+                <Link href="/recipes" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
+                {isLoggedIn && <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`}>My Profile</Link>}
             </div>
 
             <div className={styles.actions}>
             {!isLoggedIn ? (
                 <>
-                   <button className={styles.button}>Log in</button>
-                   <button className={styles.primary}>Register</button>
+                   <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`}>Log in</Link>
+                   <Link href="/register" className={styles.primary}>Register</Link>
                 </>
             ):(
                 <>
-                   <button className={styles.primary}>Add Recips</button>
-                   <div className={styles.user}>
+                   <Link href="/addRecipe" className={styles.primary}>Add Recips</Link>
+                   <div className={styles.userSection}>
+                    <div className={styles.user}>
+                    
                     {user.avatar ?(
-                        <Image src={user.avatar} alt={user.name} className={styles.avatar}/>
+                        <Image src={user.avatar} alt={displayName} width={32} height={32} className={styles.avatar}/>
                     ):(
                         <div className={styles.avatarFallback}>
-                            {user.name.charAt(0).toUpperCase()}
+                            {avatarLetter}
                         </div>
                     )}
-                    {user.name}
-                    <button className={styles.logoutBtn}><Image src="/logOut.svg" alt="Log out" width={24} height={28}/></button>
+                    <span>{displayName}</span>
+                    </div>
+                    <button className={styles.logoutBtn} onClick={()=>setLogoutOpen(true)}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>
                    </div>
                 </>
             )}
@@ -71,32 +80,34 @@ export default function Header(){
                 {!isLoggedIn ? (
             <>
               <div className={styles.mobileLink}>
-                <Link href="/recipes">Recipes</Link>
-                <Link href="/logIn">Log in</Link>
+                <Link href="/recipes"className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
+                <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`}>Log in</Link>
               </div> 
-                <button className={styles.primary}>Register</button>
+              <Link href="/register" className={styles.primary}>Register</Link>
             </>
           ) : (
             <>
-                <Link href="/recipes">Recipes</Link>
-                <Link href="/profile">My Profile</Link>
+                <Link href="/recipes" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
+                <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`}>My Profile</Link>
+                <div className={styles.userSection}>
                 <div className={styles.user}>
                     {user.avatar ?(
-                        <Image src={user.avatar} alt={user.name} className={styles.avatar}/>
+                        <Image src={user.avatar} alt={displayName} width={32} height={32} className={styles.avatar}/>
                     ):(
                         <div className={styles.avatarFallback}>
-                            {user.name.charAt(0).toUpperCase()}
+                            {avatarLetter}
                         </div>
                     )}
-                    {user.name}
-                    <button className={styles.logoutBtn}><Image src="/logOut.svg" alt="Log out" width={24} height={28}/></button>
+                    {displayName}
+                    </div>
+                    <Link href="/logout" className={styles.logoutBtn}><Image src="/logOut.svg" alt="Log out" width={24} height={28}/></Link>
                    </div>
-                <button className={styles.primary}>Add Recipe</button>
+                   <Link href="/addRecipe" className={styles.primary}>Add Recips</Link>
             </>
           )}
         </div>
       )}
-        
+        {logoutOpen && (<LogoutModal onClose={()=> setLogoutOpen(false)}/>)}
         </header>
     );
 }
