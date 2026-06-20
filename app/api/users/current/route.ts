0ae6ad35c3568e908440464c923657f55546a5ee
Viktoriia Veremiichuk <vikaveremiychuk@gmail.com>
2026-06-21 02:33:26 +0300

    Update route.ts

--- app/api/users/current/route.ts
+++ app/api/users/current/route.ts
@@ -8,6 +8,13 @@ export async function GET() {
   try {
     const cookieStore = await cookies();
 
+    const hasSession =
+      cookieStore.get("accessToken") || cookieStore.get("sessionId");
+
+    if (!hasSession) {
+      return NextResponse.json(null, { status: 200 });
+    }
+    
     const res = await api("/api/users/current", {
       headers: {
         Cookie: cookieStore.toString(),
@@ -17,6 +24,9 @@ export async function GET() {
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
     if (isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        return NextResponse.json(null, { status: 200 });
+      }
       logErrorResponse(error.response?.data);
       return NextResponse.json(
         { error: error.message, response: error.response?.data },
