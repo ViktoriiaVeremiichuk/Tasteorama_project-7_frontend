40848c89b400bc3f9fae3b15dc22e4ae288f61cd
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-21 23:48:48 +0200

    token clearing, redirection to the main page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
@@ -22,13 +22,25 @@ export default function Header(){
     useEffect(()=> {
         if (user) return; 
         async function loadUser() {
-            const res = await fetch("/app/users/current");
+            const res = await fetch("/api/users/current");
             if(!res.ok) return;
             setUser(await res.json())
         }
         loadUser();
     }, [user, setUser]);
   
+    useEffect(() => {
+        if (menuOpen) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = "auto";
+        }
+      
+        return () => {
+          document.body.style.overflow = "auto";
+        };
+      }, [menuOpen]);
+
     const displayName = user ? user.name?.trim()|| user.email.split("@")[0]: "";
     const avatarLetter = displayName ? displayName.charAt(0).toUpperCase(): ""; 
     
@@ -37,7 +49,25 @@ export default function Header(){
     const isLoginActive = pathname.startsWith("/login");
     const isProfileActive = pathname.startsWith("/profile");
     
-    const isLoggedIn = Boolean(user);   
+    //очищення токена, перенаправлення на головну сторінку при натасканні на кнопку вихід
+    const router = useRouter();
+
+    async function handleLogout() {
+        try{
+            await fetch("/api/auth/logout", {
+                method: "POST",
+            });
+        } catch(error){
+            console.error("Logout failed", error);
+        } finally {
+            setUser(null);
+            setMenuOpen(false);
+            router.push("/");
+        }
+        
+    }
+
+    const isLoggedIn = true;//Boolean(user);   
     return(
         <header className={`${styles.header} ${inter.className}`}>
         <Link href="/" className={styles.logo}>
@@ -73,7 +103,7 @@ export default function Header(){
                     )}
                     <span>{displayName}</span>
                     </div>
-                    <button className={styles.logoutBtn} onClick={()=>setLogoutOpen(true)}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>
+                    <button className={styles.logoutBtn} onClick={handleLogout}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>
                    </div>
                 </>
             )}
@@ -92,40 +122,53 @@ export default function Header(){
         {/* Mobile drawer */}
         {menuOpen && (
             <div className={styles.mobileMenu}>
+                <div className={styles.mobileTop}>
+                    <Link href="/" className={styles.logo} onClick={() => setMenuOpen(false)}>
+                        <Image src="/logo.svg" alt="Tasteorama logo" width={24} height={24} />
+                        <span>Tasteorama</span>
+                    </Link>
+
+                    <button className={styles.closeButton} onClick={() => setMenuOpen(false)}>
+                        <Image src="/close.svg" alt="Close menu" width={32} height={32} />
+                    </button>
+                </div>
+
                 {!isLoggedIn ? (
-            <>
-              <div className={styles.mobileLink}>
-                <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
-                <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`}>Log in</Link>
-              </div> 
-              <Link href="/register" className={styles.primary}>Register</Link>
-            </>
-          ) : (
-            <>
-                <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
-                <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`}>My Profile</Link>
-                <div className={styles.userSection}>
-                <div className={styles.user}>
-                    {user?.avatar ?(
-                        <Image src={user.avatar} alt={displayName} width={32} height={32} className={styles.avatar}/>
+                <>
+                    <div className={styles.mobileLink}>
+                        <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`} onClick={()=> setMenuOpen(false)}>Recipes</Link>
+                        <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`} onClick={()=> setMenuOpen(false)}>Log in</Link>
+                    </div> 
+
+                    <Link href="/register" className={styles.primary} onClick={()=> setMenuOpen(false)}>Register</Link>
+                </>
+                ) : (
+                <>
+                    <div className={styles.mobileLink}>
+                        <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`} onClick={()=> setMenuOpen(false)}>Recipes</Link>
+                        <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`} onClick={()=> setMenuOpen(false)}>My Profile</Link>
+                    </div>
+                
+                    <div className={styles.userSection}>
+                        <div className={styles.user}>
+                        {user?.avatar ?(
+                            <Image src={user.avatar} alt={displayName} width={32} height={32} className={styles.avatar}/>
                     ):(
                         <div className={styles.avatarFallback}>
                             {avatarLetter}
                         </div>
                     )}
-                    {displayName}
+                        <span>{displayName}</span>
+                        </div>
+                        <button className={styles.logoutBtn} onClick={handleLogout}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>   
                     </div>
-                    <button className={styles.logoutBtn} onClick={()=>setLogoutOpen(true)}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>
-                   </div>
-                   <Link href="/add-recipe" className={styles.primary}>Add Recipe</Link>
-            </>
-          )}
-        </div>
-      )}
-     
+                    <Link href="/add-recipe" className={styles.primary} onClick={()=> setMenuOpen(false)}>Add Recipe</Link>
+                </>
+                )}
+            </div>
+            )}
         </header>
-    );
-}
+        )}
 
 // цей код має знаходитись в кінці головного коду, перед закриттям Headera
-// {logoutOpen && (<LogoutModal onClose={()=> setLogoutOpen(false)}/>)} 
+/* {logoutOpen && (<LogoutModal onClose={()=> setLogoutOpen(false)}/>)}; */
