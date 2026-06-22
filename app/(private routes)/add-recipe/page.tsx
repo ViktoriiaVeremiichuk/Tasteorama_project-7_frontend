7fec767c8d7a5901f3d8b916362719620b3c725e
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-06-22 22:05:18 +0200

    fix title ingredients in mobile, add warner min 2 ingredients

--- app/(private routes)/add-recipe/page.tsx
+++ app/(private routes)/add-recipe/page.tsx
@@ -1,9 +1,10 @@
 import AddRecipeForm from "@/components/AddRecipeForm/AddRecipeForm";
+import styles from "./AddRecipePage.module.css";
 
 export default function AddRecipePage() {
   return (
     <main>
-      <h1>Add Recipe</h1>
+      <h1 className={styles.title}>Add Recipe</h1>
 
       <AddRecipeForm />
     </main>
