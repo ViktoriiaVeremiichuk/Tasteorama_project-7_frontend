1be9ca3da1cdbcbfb2c5124e7299dd3d67b596a5
Viktoriia Veremiichuk <vikaveremiychuk@gmail.com>
2026-06-25 10:19:29 +0300

    fex header style

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -13,163 +13,258 @@ import LogoutModal from "../Logout/LogoutModal/LogoutModal";
 import { useAuthStore } from "@/lib/store/authStore";
 import { useSearchStore } from "@/app/store/searchStore";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Header(){
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [logoutOpen, setLogoutOpen] = useState(false);
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [logoutOpen, setLogoutOpen] = useState(false);
 
-    const user = useAuthStore((state) => state.user);
-    const setUser = useAuthStore((state) => state.setUser);
-    const resetFilters = useSearchStore((state) => state.resetFilters);
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const resetFilters = useSearchStore((state) => state.resetFilters);
 
-    useEffect(() => {
-        if (user) return;
-      
-        async function loadUser() {
-          try {
-            const res = await fetch("/api/users/current", {
-              credentials: "include",
-            });
-      
-            if (!res.ok) return;
-      
-            const data = await res.json();
-      
-            if (data) {
-              setUser(data);
-            }
-          } catch (error) {
-            console.error("Failed to load user", error);
-          }
+  useEffect(() => {
+    if (user) return;
+
+    async function loadUser() {
+      try {
+        const res = await fetch("/api/users/current", {
+          credentials: "include",
+        });
+
+        if (!res.ok) return;
+
+        const data = await res.json();
+
+        if (data) {
+          setUser(data);
         }
-      
-        loadUser();
-      }, [user, setUser]);
-  
-    useEffect(() => {
-        if (menuOpen) {
-          document.body.style.overflow = "hidden";
-        } else {
-          document.body.style.overflow = "auto";
-        }
-      
-        return () => {
-          document.body.style.overflow = "auto";
-        };
-      }, [menuOpen]);
+      } catch (error) {
+        console.error("Failed to load user", error);
+      }
+    }
 
-    const displayName = user ? user.name?.trim()|| user.email.split("@")[0]: "";
-    const avatarLetter = displayName ? displayName.charAt(0).toUpperCase(): ""; 
-    
-    const pathname = usePathname();
-    const isRecipesActive = pathname === "/" || pathname.startsWith("/recipes");
-    const isLoginActive = pathname.startsWith("/login");
-    const isProfileActive = pathname.startsWith("/profile");
+    loadUser();
+  }, [user, setUser]);
 
-    const isLoggedIn =Boolean(user);  
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
-    const handleLogoClick = () => {
-        resetFilters();
-        setMenuOpen(false);
+    return () => {
+      document.body.style.overflow = "auto";
     };
+  }, [menuOpen]);
 
-    const handleProfileClick = () => {
-        resetFilters();
-        setMenuOpen(false);
-    };
-    
+  const displayName = user ? user.name?.trim() || user.email.split("@")[0] : "";
+  const avatarLetter = displayName ? displayName.charAt(0).toUpperCase() : "";
 
-    return(
-        <header className={`${styles.header} ${inter.className}`}>
-        <Link href="/" className={styles.logo} onClick={handleLogoClick}>
-            <Image src="/logo.svg" alt="Tasteorama logo" width={24} height={24}/>
-            <span>Tasteorama</span>
-        </Link>
+  const pathname = usePathname();
+  const isRecipesActive = pathname === "/" || pathname.startsWith("/recipes");
+  const isLoginActive = pathname.startsWith("/login");
+  const isProfileActive = pathname.startsWith("/profile");
 
-        {/*Desktop*/}
-        <nav className={styles.desktopNav}>
-            <div className={styles.navLinks}>
-                <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}>Recipes</Link>
-                {isLoggedIn && <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`} onClick={handleProfileClick}>My Profile</Link>}
-            </div>
+  const isLoggedIn = Boolean(user);
 
-            <div className={styles.actions}>
-            {!isLoggedIn ? (
-                <>
-                   <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`}>Log in</Link>
-                   <Link href="/register" className={styles.primary}>Register</Link>
-                </>
-            ):(
-                <>
-                   <Link href="/add-recipe" className={styles.primary}>Add Recipe</Link>
-                   <div className={styles.userSection}>
-                    <div className={styles.user}>
-                        <div className={styles.avatarFallback}>
-                            {avatarLetter}
-                        </div>
-                    <span>{displayName}</span>
-                    </div>
-                    <button className={styles.logoutBtn} onClick={() => setLogoutOpen(true)}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>
-                   </div>
-                </>
-            )}
-            </div>
-        </nav>
+  const handleLogoClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (window.location.pathname === "/") {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    resetFilters();
+    setMenuOpen(false);
+  };
 
-        {/* Mobile burger */}
-        <button className={`${styles.burger} ${menuOpen ? styles.closeButton: ""}`} onClick={() => setMenuOpen(!menuOpen)}>
-        {menuOpen ? (
-            <Image src="/close.svg" alt="Close menu" width={32} height={32}/>
-        ):(
-            <Image src="/burger.svg" alt="Open menu" width={32} height={32}/>
-        )}
-        </button>
+  const handleProfileClick = () => {
+    resetFilters();
+    setMenuOpen(false);
+  };
 
-        {/* Mobile drawer */}
-        {menuOpen && (
-            <div className={styles.mobileMenu}>
-                <div className={styles.mobileTop}>
-                    <Link href="/" className={styles.logo} onClick={handleLogoClick}>
-                        <Image src="/logo.svg" alt="Tasteorama logo" width={24} height={24} />
-                        <span>Tasteorama</span>
-                    </Link>
+  return (
+    <header className={`${styles.header} ${inter.className}`}>
+      <Link href="/" className={styles.logo} onClick={handleLogoClick}>
+        <Image src="/logo.svg" alt="Tasteorama logo" width={24} height={24} />
+        <span>Tasteorama</span>
+      </Link>
 
-                    <button className={styles.closeButton} onClick={() => setMenuOpen(false)}>
-                        <Image src="/close.svg" alt="Close menu" width={32} height={32} />
-                    </button>
+      {/*Desktop*/}
+      <nav className={styles.desktopNav}>
+        <div className={styles.navLinks}>
+          <Link
+            href="/"
+            className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}
+          >
+            Recipes
+          </Link>
+          {isLoggedIn && (
+            <Link
+              href="/profile"
+              className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`}
+              onClick={handleProfileClick}
+            >
+              My Profile
+            </Link>
+          )}
+        </div>
+
+        <div className={styles.actions}>
+          {!isLoggedIn ? (
+            <>
+              <Link
+                href="/login"
+                className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : ""}`}
+              >
+                Log in
+              </Link>
+              <Link href="/register" className={styles.primary}>
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/add-recipe" className={styles.primary}>
+                Add Recipe
+              </Link>
+              <div className={styles.userSection}>
+                <div className={styles.user}>
+                  <div className={styles.avatarFallback}>{avatarLetter}</div>
+                  <span>{displayName}</span>
                 </div>
+                <button
+                  className={styles.logoutBtn}
+                  onClick={() => setLogoutOpen(true)}
+                >
+                  <Image
+                    src="/logOut.svg"
+                    alt="Log out"
+                    width={24}
+                    height={24}
+                  />
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </nav>
 
-                {!isLoggedIn ? (
-                <>
-                    <div className={styles.mobileLink}>
-                        <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`} onClick={()=> setMenuOpen(false)}>Recipes</Link>
-                        <Link href="/login" className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : "" }`} onClick={()=> setMenuOpen(false)}>Log in</Link>
-                    </div> 
-
-                    <Link href="/register" className={styles.primary} onClick={()=> setMenuOpen(false)}>Register</Link>
-                </>
-                ) : (
-                <>
-                    <div className={styles.mobileLink}>
-                        <Link href="/" className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`} onClick={()=> setMenuOpen(false)}>Recipes</Link>
-                        <Link href="/profile" className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`} onClick={handleProfileClick}>My Profile</Link>
-                    </div>
-                
-                    <div className={styles.userSection}>
-                        <div className={styles.user}>
-                            <div className={styles.avatarFallback}>
-                            {avatarLetter}
-                            </div>
-                            <span>{displayName}</span>
-                        </div>
-                        <button className={styles.logoutBtn} onClick={()=> {setMenuOpen(false); setLogoutOpen(true);}}><Image src="/logOut.svg" alt="Log out" width={24} height={24}/></button>   
-                    </div>
-                    <Link href="/add-recipe" className={styles.primary} onClick={()=> setMenuOpen(false)}>Add Recipe</Link>
-                </>
-                )}
-            </div>
-            )}
-            {logoutOpen && (<LogoutModal onClose={()=> setLogoutOpen(false)}/>)}
-        </header>
+      {/* Mobile burger */}
+      <button
+        className={`${styles.burger} ${menuOpen ? styles.closeButton : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? (
+          <Image src="/close.svg" alt="Close menu" width={32} height={32} />
+        ) : (
+          <Image src="/burger.svg" alt="Open menu" width={32} height={32} />
         )}
+      </button>
+
+      {/* Mobile drawer */}
+      {menuOpen && (
+        <div className={styles.mobileMenu}>
+          <div className={styles.mobileTop}>
+            <Link href="/" className={styles.logo} onClick={handleLogoClick}>
+              <Image
+                src="/logo.svg"
+                alt="Tasteorama logo"
+                width={24}
+                height={24}
+              />
+              <span>Tasteorama</span>
+            </Link>
+
+            <button
+              className={styles.closeButton}
+              onClick={() => setMenuOpen(false)}
+            >
+              <Image src="/close.svg" alt="Close menu" width={32} height={32} />
+            </button>
+          </div>
+
+          {!isLoggedIn ? (
+            <>
+              <div className={styles.mobileLink}>
+                <Link
+                  href="/"
+                  className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Recipes
+                </Link>
+                <Link
+                  href="/login"
+                  className={`${styles.loginLink} ${isLoginActive ? styles.activeLink : ""}`}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Log in
+                </Link>
+              </div>
+
+              <Link
+                href="/register"
+                className={styles.primary}
+                onClick={() => setMenuOpen(false)}
+              >
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className={styles.mobileLink}>
+                <Link
+                  href="/"
+                  className={`${styles.navLink} ${isRecipesActive ? styles.activeLink : ""}`}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Recipes
+                </Link>
+                <Link
+                  href="/profile"
+                  className={`${styles.navLink} ${isProfileActive ? styles.activeLink : ""}`}
+                  onClick={handleProfileClick}
+                >
+                  My Profile
+                </Link>
+              </div>
+
+              <div className={styles.userSection}>
+                <div className={styles.user}>
+                  <div className={styles.avatarFallback}>{avatarLetter}</div>
+                  <span>{displayName}</span>
+                </div>
+                <button
+                  className={styles.logoutBtn}
+                  onClick={() => {
+                    setMenuOpen(false);
+                    setLogoutOpen(true);
+                  }}
+                >
+                  <Image
+                    src="/logOut.svg"
+                    alt="Log out"
+                    width={24}
+                    height={24}
+                  />
+                </button>
+              </div>
+              <Link
+                href="/add-recipe"
+                className={styles.primary}
+                onClick={() => setMenuOpen(false)}
+              >
+                Add Recipe
+              </Link>
+            </>
+          )}
+        </div>
+      )}
+      {logoutOpen && <LogoutModal onClose={() => setLogoutOpen(false)} />}
+    </header>
+  );
+}
