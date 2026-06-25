2c5fe1c45281facc4c553e50b2baa2c04f69a573
Viktoriia Veremiichuk <vikaveremiychuk@gmail.com>
2026-06-25 20:02:35 +0300

    Update NotFoundRecipe.tsx

--- components/NotFoundRecipe/NotFoundRecipe.tsx
+++ components/NotFoundRecipe/NotFoundRecipe.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./NotFoundRecipe.module.css";
 
 export default function NotFoundRecipe() {
   return (
     <main>
-      <div className="not-found-recipe-container">
+      <div className={styles.container}>
         <Image
           src="/not-found.jpg"
           alt="Not Found Recipe"
           width={600}
           height={438}
-          className="not-found-recipe-image"
+          className={styles.image}
         />
-        <h1 className="not-found-recipe-title">404</h1>
-        <p className="not-found-recipe-text">Recipe not found</p>
-        <Link href="/" className="not-found-recipe-link">
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.p}>Recipe not found</p>
+        <Link href="/" className={styles.link}>
           <Image
             src="/arrow.svg"
-            className="not-found-recipe-icon"
+            className={styles.icon}
             alt="arrow"
             width={24}
             height={24}
